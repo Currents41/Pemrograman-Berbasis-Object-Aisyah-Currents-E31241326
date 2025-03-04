7b8d293e3cf52031a49e01ea0a0ae7fa8d5263e3
Currents <currentsaisyah@gmail.com>
2025-03-03 19:57:32 -0800

    Delete Zaaramedia(for team) directory

--- Zaaramedia(for team)/src/com/service/ServiceReport.java
+++
@@ -1,8 +0,0 @@
-package com.service;
-
-import javax.swing.JPanel;
-
-public interface ServiceReport {
-    void cetakBarcode(JPanel pn, String barcode);
-    void printBarcode(String barcode);    
-}
